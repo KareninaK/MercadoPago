b87873a70a55d6cc81d06c75585d6a9e4afaf089
KareninaK <65468357+KareninaK@users.noreply.github.com>
2020-07-24 16:08:09 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 define("public_key", "APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626");
-define("access_token", "APP_USR-1809569920933245-042220-57ab80259e08082e2308bf3748325ece-246910716");
+define("access_token", "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 ?>
